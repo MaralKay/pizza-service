d3aa0e4c2f215b6bb68999f8b701339c19e06b66
MaralKay <maralkou93@gmail.com>
2018-08-16 20:10:24 +0200

    initial commit

---
+++ env_pizzaService/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\maral\documents\pizzaservice\env_pizzaservice\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
